f97a47b3d379a84e8ec04733509d0c6266b66c69
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-06-23 21:20:47 +0530

    public folder changes

--- src/index.js
+++ src/index.js
@@ -71,9 +71,12 @@ app.get("/api/orders", async (req, res) => {
   }
 });
 
-app.use(express.static('../public'));
 
+app.use(express.static('public'));
 
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname + '/public/index.html'))
+// })
 
 app.get('*', (req, res) => {
   res.status(404).json({ error: '404! Not Found' });
