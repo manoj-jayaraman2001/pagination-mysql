32ad8bad77f9e42cac4d05832184a72dc5a737d6
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-06-23 16:58:26 +0530

    sql queries

--- src/connector.js
+++ src/connector.js
@@ -12,12 +12,14 @@ var con = mysql.createConnection({
 });
 
 
-con.connect(function (err) {
-    if (err)  return console.log("failed to connect to mysql server/ database", err);
-    else  return console.log("connection establish with Datebase!!!!");
+
+const fs = require('fs');
+const script = fs.readFileSync('createDatabase.js', 'utf8');
+
+connection.query(script, (error, results) => {
+  if (error) throw error;
+  console.log('Script executed successfully');
+  // Proceed with deployment
 });
-
-
-
 
 module.exports = con;
