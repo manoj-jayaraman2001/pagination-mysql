6b2cd1646dbd1b2a91f1bb32d6e5108b19bafa2b
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-06-24 06:55:45 +0530

    changes in port

--- src/index.js
+++ src/index.js
@@ -82,6 +82,6 @@ app.get('*', (req, res) => {
   res.status(404).json({ error: '404! Not Found' });
 })
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, `0.0.0.0`,() => console.log(`App listening on port ${port}!`));
 
 module.exports = app
